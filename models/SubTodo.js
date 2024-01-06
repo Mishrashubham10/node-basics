cdd6169597e3794176bd2aa583c26237ff808abb
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-06 14:12:07 +0530

    Creates subtodos model

--- models/SubTodo.js
+++ models/SubTodo.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
-const subTodoSchema = new mongoose.Schema({
-    
-}, {
-    timestamps: true
-});
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const SubTodo = mongoose.model('Todo', subTodoSchema);
 
